26007b0fd3a1710fdf78deef653acd0ff3c7931d
Developer Localhost <developer@local.host>
2024-04-10 22:08:52 -0500

    feat: Add complex test.

--- src/tests/e2e_tests_backend.test.ts
+++ src/tests/e2e_tests_backend.test.ts
@@ -7,3 +7,23 @@ test("should handle a basic greeting", async () => {
   const result1 = await e2e_tests_backend.greet("test");
   expect(result1).toBe("Hello, test!");
 });
+
+test("Should contain a candid interface", async () => {
+  const agent = Actor.agentOf(e2e_tests_backend) as HttpAgent;
+  const id = Principal.from(e2e_tests_backendCanister);
+
+  const canisterStatus = await CanisterStatus.request({
+    canisterId: id,
+    agent,
+    paths: ["time", "controllers", "candid"],
+  });
+
+  expect(canisterStatus.get("time")).toBeTruthy();
+  expect(Array.isArray(canisterStatus.get("controllers"))).toBeTruthy();
+  expect(canisterStatus.get("candid")).toMatchInlineSnapshot(`
+    "service : {
+      greet: (text) -> (text) query;
+    }
+    "
+  `);
+});
